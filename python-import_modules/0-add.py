35cfc4f8964d0a28640729776df1ebefcb2a88f9
LukeRRT <aumrungruj.t@gmail.com>
2024-05-08 11:21:49 +0700

    Task0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    from add_0 import add
 
-result = add(1, 2)
-print("{} + {} = {}".format(1, 2, result))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
