ca5f91453f0f0bc01fa465fc285b1d5193d6ed17
LukeRRT <aumrungruj.t@gmail.com>
2024-05-12 11:29:09 +0700

    Task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     if num_args == 0:
         print("0 arguments.")
     else:
-        print("{} arguments{}:".format(num_args, 's' if num_args > 1 else ''))
+        print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
