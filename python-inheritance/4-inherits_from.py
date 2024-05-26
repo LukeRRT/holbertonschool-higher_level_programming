82364b5d4c773cfe44b767aac00d633474357ac3
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 17:48:52 +0700

    Task4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,9 +3,10 @@
 Module to check subclass
 """
 
+
 def inherits_from(obj, a_class):
     """
     Returns True if obj is an instance of a class that
     inherited (directly or indirectly), otherwise False.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
