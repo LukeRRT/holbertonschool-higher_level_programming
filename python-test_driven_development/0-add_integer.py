2ea191f9ceb8a71b0aa2ceec1215b98f0d27a192
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 18:11:23 +0700

    Task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+"""
+add_integer: module that adds two integers together
+"""
+
+
 def add_integer(a, b=98):
+    """
+    A function that adds two integers
+
+    Args:
+        int a: The first integer to add.
+        int b: The second integer to add, defaults to 98.
+
+    Returns:
+        The sum of a + b
+
+    Raises:
+        TypeError: If either a or b is not an integer.
+
+    Unit tests: located in tests/0-add_integer.txt
+    """
+
     # Check if a is an integer or float
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
