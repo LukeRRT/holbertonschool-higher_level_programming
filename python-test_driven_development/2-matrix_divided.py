e399fca42a34616108714753cb48277df59c690c
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 18:14:31 +0700

    Task2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -32,8 +32,9 @@ def matrix_divided(matrix, div):
     
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     return [[round(ele / div, 2) for ele in row] for row in matrix]
+
 
 if __name__ == "__main__":
     import doctest
