a8240c205b80f8ffda7a859d4c7e58a4a85e23c2
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 18:38:27 +0700

    Task0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Noname
+"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    abstract class Animal
+    """
+    @abstractmethod
+    def sound(self):
+        """
+        abstract method sound
+        """
+        pass
+
+class Dog(Animal):
+    """
+    subclass Dog
+    """
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """
+    subclass Cat
+    """
+    def sound(self):
+        return "Meow"
