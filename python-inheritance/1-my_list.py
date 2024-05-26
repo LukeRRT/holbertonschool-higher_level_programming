f9ebeab1d7f69988019ba26e81610b8bb4629077
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 17:11:28 +0700

    Task1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """Prints the list sorted in ascending order."""
+        sorted_list = sorted(self)
+        print(sorted_list)
