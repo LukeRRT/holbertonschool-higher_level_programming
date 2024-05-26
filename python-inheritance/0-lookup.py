628d129fb7e333a3b1c5c5dfeb185b4d2c837003
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 17:07:28 +0700

    Task0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Module to return list of available attributes
+and methods of an object
+"""
+
+
 def lookup(obj):
     """Returns a list of available attributes and methods of an object."""
     return dir(obj)
