80759d5e5b51e805cf6ab61fb8447e0e48f539b5
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 18:06:19 +0700

    Task9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -5,6 +5,7 @@ class Rectangle that inherits from BaseGeometry
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry."""
 
