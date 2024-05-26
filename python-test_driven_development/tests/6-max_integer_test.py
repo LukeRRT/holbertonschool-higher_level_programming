9dcc18bf8b19f29b72984e66efda32961687cdfd
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 18:29:44 +0700

    update

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,48 +1,29 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+Unittest for max_integer([..])
 """
 import unittest
-from 6-max_integer import max_integer
+max_integer = __import__("6-max_integer").max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def test_positive_integers(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
-    def test_negative_integers(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
+    def test_max_end(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
 
-    def test_mixed_integers(self):
-        self.assertEqual(max_integer([-1, 2, 3, -4]), 3)
-        self.assertEqual(max_integer([1, -3, 4, -2]), 4)
+    def test_max_start(self):
+        self.assertEqual(max_integer([3, 2, 1]), 3)
 
-    def test_single_element(self):
-        self.assertEqual(max_integer([1]), 1)
-        self.assertEqual(max_integer([-1]), -1)
+    def test_max_middle(self):
+        self.assertEqual(max_integer([1, 3, 2]), 3)
 
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
+    def test_negative_exists(self):
+        self.assertLessEqual(max_integer([-1, -2, -3]), 0)
 
-    def test_floats(self):
-        self.assertEqual(max_integer([1.1, 2.2, 3.3, 4.4]), 4.4)
-        self.assertEqual(max_integer([1.1, 3.3, 4.4, 2.2]), 4.4)
+    def test_only_one(self):
+        self.assertEqual(max_integer([100]), 100)
 
-    def test_mixed_types(self):
-        self.assertEqual(max_integer([1, 2.2, 3, 4.4]), 4.4)
-        self.assertEqual(max_integer([-1, 3.3, 4, -2.2]), 4)
-
-    def test_identical_elements(self):
-        self.assertEqual(max_integer([7, 7, 7, 7]), 7)
-        self.assertEqual(max_integer([0, 0, 0, 0]), 0)
-
-    def test_none(self):
-        with self.assertRaises(TypeError):
-            max_integer(None)
-
-    def test_non_numeric(self):
-        with self.assertRaises(TypeError):
-            max_integer(["a", "b", "c"])
+    def test_empty(self):
+        self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
     unittest.main()
