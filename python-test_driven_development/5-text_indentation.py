0d8042fe2a2b6e1d272955949abbe723c3ef4c10
LukeRRT <aumrungruj.t@gmail.com>
2024-05-26 15:35:07 +0700

    Task5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    special_chars = {'.', '?', ':'}
+    result = ""
+    i = 0
+    while i < len(text):
+        result += text[i]
+        if text[i] in special_chars:
+            result += "\n\n"
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+                continue
+        i += 1
+    print(result, end="")
