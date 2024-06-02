405040c6acc70c7d491a779000d0038585a6a01f
LukeRRT <aumrungruj.t@gmail.com>
2024-06-02 15:48:58 +0700

    Task4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a function to return JSON representation by Python data
+structure
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string."""
+    return json.loads(my_str)
