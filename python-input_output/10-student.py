7887df611b93ab118118b0df14af4b544b4852b8
LukeRRT <aumrungruj.t@gmail.com>
2024-06-02 16:34:02 +0700

    Task10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance
+        with the given attributes.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation
+        of the Student instance.
+        
+        If attrs is a list of strings, only attributes
+        names contained in this list will be retrieved.
+        Otherwise, all attributes will be retrieved.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in
+                    self.__dict__.items() if key in attrs}
