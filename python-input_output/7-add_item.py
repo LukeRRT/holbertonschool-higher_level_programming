9ebbb15997e6c39f42eb9334bce6c17278636b45
LukeRRT <aumrungruj.t@gmail.com>
2024-06-02 16:13:16 +0700

    Task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """
+This script adds all command-line arguments to a Python list,
+and then saves them to a file named add_item.json.
 
+It uses the functions save_to_json_file and load_from_json_file
+for handling JSON data.
 """
 import sys
 import os
